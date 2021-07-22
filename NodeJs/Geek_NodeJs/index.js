c66e31d4f8648c90bbde2d9fba41231a40f46b88
zhangya <zhangya3@mychery.com>
2021-07-22 14:00:43 +0800

    Node.js学习demo添加剪刀石头布小游戏

---
+++ NodeJs/Geek_NodeJs/index.js
@@ -0,0 +1,31 @@
+// console.log(process.argv)
+
+const playerAction = process.argv[process.argv.length -1]
+
+console.log(playerAction)
+
+const random = Math.random() * 3
+
+var computerAction = ''
+
+if(random < 1) {
+    computerAction = 'rock'
+} else if(random > 2) {
+    computerAction = 'paper'
+} else {
+    computerAction = 'scissor'
+}
+console.log(computerAction)
+if(playerAction == computerAction) {
+    console.log('平局')
+} else if(
+    (playerAction === 'rock' && computerAction === 'scissor') ||
+    (playerAction === 'rock' && computerAction === 'scissor') ||
+    (playerAction === 'rock' && computerAction === 'scissor')
+) {
+    console.log('你赢了')
+} else {
+    console.log('你输了')
+}
+
+
